24eec6f39f405bf24fe6989b35385d24ebf3f93e
Jessriel <jamillajessriel17@gmail.com>
2021-12-16 16:39:32 +0800

    change style

--- src/app/components/about-me-component/about-me-component.component.ts
+++ src/app/components/about-me-component/about-me-component.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-about-me-component',
   templateUrl: './about-me-component.component.html',
   styleUrls: ['./about-me-component.component.css']
 })
 export class AboutMeComponentComponent implements OnInit {
-
-  constructor() { }
+  aboutMe:any = {
+    birthday: "February 17, 1998",
+    age: 24,
+    address: "Gloria, Oriental Mindoro",
+    nationality: "Filipino"
+  }
+  constructor() { 
+ 
+  }
 
   ngOnInit(): void {
+
   }
 
 }
